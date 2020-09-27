35575ff195fa0c3a77429021e9aa90764987e8c1
Jeremy Strader <tempest.nano@gmail.com>
2020-09-27 00:12:32 -0400

    fixed multiarch again

---
+++ Buildme.sh
@@ -0,0 +1,3 @@
+docker build -t jstrader/docker-wireguard-pia:arm32v7 -f .\Dockerfile.arm32v7 ./
+docker build -t jstrader/docker-wireguard-pia:arm64v8 -f .\Dockerfile.arm64v8 ./
+docker build -t jstrader/docker-wireguard-pia:amd64 -f .\Dockerfile ./
