9424f46764c3b03933744f6b702f8179f4cea064
Jeremy Strader <tempest.nano@gmail.com>
2020-09-27 10:54:49 -0400

    test5

--- Buildme.sh
+++ Buildme.sh
@@ -1,3 +1,4 @@
 docker build -t jstrader/docker-wireguard-pia:arm32v7 -f .\Dockerfile.arm32v7 ./
 docker build -t jstrader/docker-wireguard-pia:arm64v8 -f .\Dockerfile.arm64v8 ./
 docker build -t jstrader/docker-wireguard-pia:amd64 -f .\Dockerfile ./
+docker build -t jstrader/docker-wireguard-pia:test -f .\Dockerfile.test ./
